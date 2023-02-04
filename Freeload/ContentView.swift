b4fe612b0fdbb4236801dd5a4bfe841bc7c19bc1
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-02-04 11:52:26 -0500

    It's aliiivee!!!

--- Freeload/ContentView.swift
+++ Freeload/ContentView.swift
@@ -8,20 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var inProgress = false
     let appVersion = ((Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "Unknown") + " (" + (Bundle.main.infoDictionary?["CFBundleVersion"] as? String ?? "Unknown") + ")")
     var body: some View {
         NavigationView {
             List {
                 Section {
                     Button(action: {
-                        print("test")
                         Haptic.shared.play(.heavy)
+                        inProgress = true
+                        let success = patch_installd()
+                        if success {
+                            UIApplication.shared.alert(title: "Success", body: "Successfully patched installd!", withButton: true)
+                            Haptic.shared.notify(.success)
+                        } else {
+                            UIApplication.shared.alert(title: "Failure", body: "Failed to patch installd!", withButton: true)
+                            Haptic.shared.notify(.error)
+                        }
                     }, label: {
                         Label("Apply", systemImage: "checkmark.seal")
                     }
                     )
                 }
-                Section(header: Text("Freeload " + appVersion + "\nMade with ❤️ by BomberFish")) {}.textCase(nil)
+                Section(header: Text("Freeload " + appVersion + "\nMade with ❤️ by BomberFish\nThanks @zhuowei for the installd patch")) {}.textCase(nil)
                     .navigationTitle("Freeload")
             }
         }
