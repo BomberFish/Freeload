dff0d4d90e31462d01013c6710c021fe20f78ca3
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-02-04 11:12:23 -0500

    Initial Commit

---
+++ Freeload/FreeloadApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreeloadApp.swift
+//  Freeload
+//
+//  Created by Hariz Shirazi on 2023-02-04.
+//
+
+import SwiftUI
+
+@main
+struct FreeloadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
